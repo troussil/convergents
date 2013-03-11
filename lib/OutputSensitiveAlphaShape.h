e50b1ba1a53271133cfb6b9a33b384681487a41b
Homeostasie <thom@homeostasie.eu>
2013-03-11 18:12:10 +0100

    Output sensitive incremental

--- lib/OutputSensitiveAlphaShape.h
+++ lib/OutputSensitiveAlphaShape.h
@@ -215,91 +215,91 @@ class OutputSensitiveAlphaShape
 
               if ( k % 2 != 0 || (pConv == aPointb && k % 2 == 0))
               {
-          /**
-           * We test the parity of k :
-           * In the case, k is even, we follow searching for new convergent.
-           * In the other case, ie : k is odd, we are above the straight-line. We could  
-           * have new alpha-shape vertices.
-           */ 
+                /**
+                 * We test the parity of k :
+                 * In the case, k is even, we follow searching for new convergent.
+                 * In the other case, ie : k is odd, we are above the straight-line. We could  
+                 * have new alpha-shape vertices.
+                 */ 
 
-          if (aPredicate(pStart, pConv-vConvM1, pConv) == false)
-          {
-            /**
-             * In the family of triangle shaped by the three points : pStart, 
-             * pConv - q*vConvM1, pConv - (q+1)*vConvM1, the triangle T(pStart, 
-             pConv-vConvM1, pConv) have the greatest circumcircle radius.
-             * If its radius is smaller than the predicate radius, we have to search
-             * for new vertices.
-             * We throw the dichotomous method in order to find the first point
-             * in the alpha-shape.
-             */
+                if (aPredicate(pStart, pConv-vConvM1, pConv) == false)
+                {
+                  /**
+                   * In the family of triangle shaped by the three points : pStart, 
+                   * pConv - q*vConvM1, pConv - (q+1)*vConvM1, the triangle T(pStart, 
+                   pConv-vConvM1, pConv) have the greatest circumcircle radius.
+                   * If its radius is smaller than the predicate radius, we have to search
+                   * for new vertices.
+                   * We throw the dichotomous method in order to find the first point
+                   * in the alpha-shape.
+                   */
 
-           
 
-						if (pConv == aPointb && k % 2 == 0)
-						{
-						 	
-						 	qkalpha = dichotomous(aPredicate, pConvM2, -vConvM2, vConvM1, qk);
-							
-							int qks = qkalpha;
-							qkalpha = 1;
-							
-				      while ( qkalpha <= qk-qks)
-				      {
-				        *aAlphaShapeHull++ = pStart + qkalpha*vConvM1;
-				        qkalpha++;
-				        
-				      }
-				      // We can have a new vertex between in aPointb - vConvM2.
-				      if (qks == 0 && aPredicate.getNum2() < aPredicate.getDen2())
-				      {
-				   	    *aAlphaShapeHull++ = (pConv - vConvM2);
-				      }
-				      nextVertex = true;
-              *aAlphaShapeHull++ = aPointb;
-              pStart = aPointb;
-						}
-						else
-						{
-							qkalpha = dichotomous(aPredicate, pStart, vConvM2, vConvM1, qk);
-		          if (qkalpha == 0)
-		          {
-		            /**
-		             * If qkalpha == 0, we have to deal with special case.
-		             * In every case, pConvM2 is a new vertex.
-		             */
-		            *aAlphaShapeHull++ = pConvM2;
 
-		            if (pConvM1.normL22() == 1)
-		            {
-		              // We can have a new vertex between pConvM2 and pConv.
-		              *aAlphaShapeHull++ = (pConvM2 + vConvM1);
-		              pStart = (pConvM2 + vConvM1);
-		            }
-		            else
-		            {
-		              pStart = pConvM2;
-		            }
-		          }
-		          else
-		          {
-		            /**
-		             * We add all the vertices between qkalpha and qk in the alpha-Shape.
-		             * We restart from the last vertex add : pConv.
-		             */
-		            while (qkalpha <= qk)
-		            {
-		              *aAlphaShapeHull++ = pConvM2 + qkalpha*vConvM1;
-		              qkalpha++;  
-		            }
-		            pStart = pConv;
+                  if (pConv == aPointb && k % 2 == 0)
+                  {
 
-		          }
-		          // We have to reset the convergent computation from pStart
-		          nextVertex = true;
-            }
-          } // if new vertex. 
-        } // if k is odd.
+                    qkalpha = dichotomous(aPredicate, pConvM2, -vConvM2, vConvM1, qk);
+
+                    int qks = qkalpha;
+                    qkalpha = 1;
+
+                    while ( qkalpha <= qk-qks)
+                    {
+                      *aAlphaShapeHull++ = pStart + qkalpha*vConvM1;
+                      qkalpha++;
+
+                    }
+                    // We can have a new vertex between in aPointb - vConvM2.
+                    if (qks == 0 && aPredicate.getNum2() < aPredicate.getDen2())
+                    {
+                      *aAlphaShapeHull++ = (pConv - vConvM2);
+                    }
+                    nextVertex = true;
+                    *aAlphaShapeHull++ = aPointb;
+                    pStart = aPointb;
+                  }
+                  else
+                  {
+                    qkalpha = dichotomous(aPredicate, pStart, vConvM2, vConvM1, qk);
+                    if (qkalpha == 0)
+                    {
+                      /**
+                       * If qkalpha == 0, we have to deal with special case.
+                       * In every case, pConvM2 is a new vertex.
+                       */
+                      *aAlphaShapeHull++ = pConvM2;
+
+                      if (pConvM1.normL22() == 1)
+                      {
+                        // We can have a new vertex between pConvM2 and pConv.
+                        *aAlphaShapeHull++ = (pConvM2 + vConvM1);
+                        pStart = (pConvM2 + vConvM1);
+                      }
+                      else
+                      {
+                        pStart = pConvM2;
+                      }
+                    }
+                    else
+                    {
+                      /**
+                       * We add all the vertices between qkalpha and qk in the alpha-Shape.
+                       * We restart from the last vertex add : pConv.
+                       */
+                      while (qkalpha <= qk)
+                      {
+                        *aAlphaShapeHull++ = pConvM2 + qkalpha*vConvM1;
+                        qkalpha++;  
+                      }
+                      pStart = pConv;
+
+                    }
+                    // We have to reset the convergent computation from pStart
+                    nextVertex = true;
+                  }
+                } // if new vertex. 
+              } // if k is odd.
 
               // Update Convergent
               k++;
@@ -435,5 +435,214 @@ class OutputSensitiveAlphaShape
       while(myShape(pNext+vNext) >= 0){pNext += vNext;}
       return pNext; 
     }
+
+    /**
+     * Given a vertex of the alpha-shape, find a serie of vertices
+     * in a counter-clockwise order
+     * 
+     * @param aPredicate determine the alpha-shape radius
+     * @param aPoint any vertex of the alpha-shape 
+     * @param aAlphaShape Output Iterator which log a serie of vertices
+     * @return the last vertex log by the output iterator
+     */
+    template <typename CircumcircleRadiusPredicate, typename Point, typename OutputIterator>
+      Point nextLeftInc(const CircumcircleRadiusPredicate& aPredicate, const Point& aPointa, int aMaxConv, OutputIterator aAlphaShapeHull)
+      {
+        //std::cout<<std::endl;   
+        //std::cout<<"#0 - Enter : pStart :"<<aPointa<<std::endl;
+        // Initialisation of the convergent.
+        // First convergents vectors : 
+        int rot_pi2[4];
+        rot_pi2[0] = 0; rot_pi2[1] = -1; rot_pi2[2] = 1; rot_pi2[3] = 0;
+
+        Point vConvM2 = Point(1,0);
+        Point vConvM1 = Point(0,1);
+        Point vConv   = Point(0,0);
+
+        // Orientation of the convergent
+        // vm2 outside and vm1 inside
+        while (myShape(aPointa + vConvM2) > 0 || myShape(aPointa + vConvM1) < 0)
+        {
+          // pi/2 counter clockwise rotation
+          vConvM2 = vConvM2.rotate(rot_pi2);
+          vConvM1 = vConvM1.rotate(rot_pi2);
+        }
+
+        // First convergents
+        Point pConvM2 = aPointa + vConvM2;
+        Point pConvM1 = aPointa + vConvM1;
+        // pConv is the next convergent pconv = pConvM2 + qk * vConvM1.
+
+        Point pConv;
+        int qk;
+
+        // pConvM2 + qkalpha * vConvM1 is the first vertex in the alpha shape.
+        int qkalpha;
+
+        // True : We have add new vertex/ices for this convergent.
+        bool enter = true;
+
+        // k is the convergent number. Usefull to know if the convergent is odd or even
+        // ie : if the convergent is below or above the straight line
+        int k = 0;
+
+        /**
+         * p0 does not exist.
+         * We can only translate in the pConvM1 direction.
+         * We could add the translation in one time.
+         * ============================================= 
+         */
+        if (myShape.dray(pConvM2, vConvM1, qk, pConv) == false)
+        {
+          // We search for the next vertices in vConvM1 direction
+          pConv = pConvM1;
+          // Maximum length to the next alpha-neighbour.
+          int kmax = aPredicate.getNum2()/aPredicate.getDen2();
+
+          // VConvM1 translation
+          while(myShape(pConv + vConvM1) >= 0)
+          {
+            pConv += vConvM1;
+            k++;
+            if (k == kmax)
+            {// We need an alpha-neighbour to reach the end of the straight-line.
+              *aAlphaShapeHull++ = pConv;
+              k = 0;
+            }
+          }
+          // We add the last vertex
+          if (k != 0){*aAlphaShapeHull++ = pConv;}
+          // We found at least one new vertex.
+          return (pConv);
+        } // end - p_0 does not exist
+        // The else is not need because of the return.
+        /*
+         * =============================================
+         */ 
+
+
+        /**
+         * We start searching for next vertices and convergents.
+         * Everytime, we found a set of vertices, we restart this procedure.
+         * It also stop if there is no intersection between the ray and the shape.
+         */ 
+
+        while (myShape.dray(pConvM2, vConvM1, qk, pConv) == true)
+        {
+          // pConv is calculate in lineRatio.dray(), so we update vConv
+          vConv = pConv - aPointa;
+          if (k % 2 == 0 && myShape(pConv) >=0)
+          {  
+          	//std::cout<<"#3.2 - paire - k : "<<k<<"| pConv : "<<pConv<<", pConvM2, pConvM1"<<pConvM2<<pConvM1<<std::endl;
+            //std::cout<<"#3.2 - entrée calcul : "<<myShape(pConv)<<" resul : "<<aPredicate(aPointa, pConv-vConvM1, pConv)<<"; k = "<<k<<std::endl;  
+            qkalpha = dichotomous(aPredicate, pConvM2, -vConvM2, vConvM1, qk);
+            int qks = qkalpha;
+            qkalpha = 1;
+
+            while ( qkalpha <= qk-qks)
+            {
+              *aAlphaShapeHull++ = aPointa + qkalpha*vConvM1;
+              qkalpha++;
+
+            }
+            // We can have a new vertex between in aPointb - vConvM2.
+            if (qks == 0 && aPredicate.getNum2() < aPredicate.getDen2())
+            {
+              *aAlphaShapeHull++ = (pConv - vConvM2);
+            }
+            *aAlphaShapeHull++ = pConv;
+            return pConv;
+          }
+
+          if ( k % 2 != 0 )
+          {
+            /**
+             * We test the parity of k :
+             * In the case, k is even, we follow searching for new convergent.
+             * In the other case, ie : k is odd, we are above the straight-line. 
+             * We could have new alpha-shape vertices if pConv lie on the shape.
+             */ 
+            //std::cout<<"#3.1 - parité - k : "<<k<<"| pConv : "<<pConv<<", pConvM2, pConvM1"<<pConvM2<<pConvM1<<std::endl;  
+            //std::cout<<"#3.1 - entrée calcul : "<<myShape(pConv)<<" resul : "<<aPredicate(aPointa, pConv-vConvM1, pConv)<<"; k = "<<k<<std::endl;  
+            if (aPredicate(aPointa, pConv-vConvM1, pConv) == false)
+            {
+              /**
+               * In the family of triangle shaped by the three points : pStart, 
+               * pConv - q*vConvM1, pConv - (q+1)*vConvM1, the triangle T(pStart, 
+               * pConv-vConvM1, pConv) have the greatest circumcircle radius.
+               * If its radius is smaller than the predicate radius, we have to search
+               * for new vertices.
+               * We throw the dichotomous method in order to find the first point
+               * in the alpha-shape.
+               */
+
+              //std::cout<<"#3.3 - impaire - k : "<<k<<"| pConv : "<<pConv<<", pConvM2, pConvM1"<<pConvM2<<pConvM1<<std::endl;
+              //std::cout<<"#3.3 - entrée calcul : "<<myShape(pConv)<<" resul : "<<aPredicate(aPointa, pConv-vConvM1, pConv)<<"; k = "<<k<<std::endl;  
+              qkalpha = dichotomous(aPredicate, aPointa, vConvM2, vConvM1, qk);
+              if (qkalpha == 0)
+              {
+                /**
+                 * If qkalpha == 0, we have to deal with special case.
+                 * In every case, pConvM2 is a new vertex.
+                 */
+                *aAlphaShapeHull++ = pConvM2;
+
+                if (pConvM1.normL22() == 1)
+                {
+                  // We can have a new vertex between pConvM2 and pConv.
+                  *aAlphaShapeHull++ = (pConvM2 + vConvM1);
+                  return(pConvM2 + vConvM1);
+                }
+                else
+                {
+                  return(pConvM2);
+                }
+              }
+              else
+              {
+                /**
+                 * We add all the vertices between qkalpha and qk in the alpha-Shape.
+                 * We restart from the last vertex add : pConv.
+                 */
+                while (qkalpha <= qk)
+                {
+                  *aAlphaShapeHull++ = pConvM2 + qkalpha*vConvM1;
+                  qkalpha++;  
+                }
+                return(pConv);
+              }
+              // We have to reset the convergent computation from pStart
+
+            } // if new vertex. 
+          } // if k is odd.
+
+          // Update Convergent
+          k++;
+
+          pConvM2 = pConvM1;
+          pConvM1 = pConv;
+          vConvM2 = vConvM1;
+          vConvM1 = pConv-aPointa;
+        } // No new convergent or new vertex
+
+        /**
+         * No intersection
+         * We add and return the last convergent to the alpha-shape.
+         */ 
+
+        if (myShape(pConvM2) > 0)
+        {
+        	//std::cout<<"#2 - Paire : pConvM1 : "<<k<<pConvM1<<std::endl;  
+          *aAlphaShapeHull++ = pConvM1;
+          return pConvM1;
+        }
+        else
+        {
+        	//std::cout<<"#2 - Paire: pConv : "<<pConv<<std::endl;  
+          *aAlphaShapeHull++ = pConvM2;
+          return pConvM2;
+        }
+      } // end - proc
+
 }; 
 #endif
--- src/testAlphaShape.cpp
+++ src/testAlphaShape.cpp
@@ -18,7 +18,7 @@
 //////////////////////////////////////////////////////////////////////
 template <typename Shape, typename Point, typename OutputIterator, 
   typename Predicate>
-void alphaShape(const Shape& aShape, const Point& aStartingPoint, 
+void alphaShapeNaive(const Shape& aShape, const Point& aStartingPoint, 
     OutputIterator res, int aMaxConv, const Predicate& aPredicate)
 {
   OutputSensitiveAlphaShape<Shape, Predicate> ch(aShape, aPredicate);
@@ -27,12 +27,9 @@ void alphaShape(const Shape& aShape, const Point& aStartingPoint,
   Point tmp1 = aStartingPoint; 
   Point tmp2;
 
-  int i = 0;
   *res++ = tmp1; 
   while (tmp2 != aStartingPoint)
   {
-    //store the current vertex
-
 
     //get the next convex hull vertex
     tmp2 = ch.next(tmp1);
@@ -42,9 +39,28 @@ void alphaShape(const Shape& aShape, const Point& aStartingPoint,
     tmp1 = tmp2;
     //while it is not the first one
   }
-
 }
 
+//////////////////////////////////////////////////////////////////////
+template <typename Shape, typename Point, typename OutputIterator, 
+  typename Predicate>
+void alphaShape(const Shape& aShape, const Point& aStartingPoint, 
+    OutputIterator res, int aMaxConv, const Predicate& aPredicate)
+{
+  OutputSensitiveAlphaShape<Shape, Predicate> ch(aShape, aPredicate);
+
+  //get the first vertex
+  Point tmp = aStartingPoint; 
+	int k = 0;
+	
+  *res++ = tmp; 
+  while (tmp != aStartingPoint || k == 0)
+  {
+		k++;
+    // get the next alpha-shape vertices
+    tmp = ch.nextLeftInc(aPredicate, tmp, aMaxConv, res);
+  }//while we not return to aStartingPoint
+}
 
 ///////////////////////////////////////////////////////////////////////
 int main() 
@@ -56,6 +72,153 @@ int main()
   int nbok = 0; //number of tests ok
   int nb = 0;   //total number of tests
   std::cout << std::endl; 
+  
+  std::cout << "I) Alpha-shape on a simple circle - Naïve" << std::endl; 
+  {
+
+    Circle circle( Point(5,0), Point(0,5), Point(-5,0) );
+
+    std::vector<Point> boundary; 
+    Vector dir(1,0); 
+    closedTracking( circle, circle.getConvexHullVertex(), dir, std::back_inserter(boundary) ); 
+    std::cout << "Boundary" << std::endl; 
+    std::copy(boundary.begin(), boundary.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+    std::cout << std::endl; std::cout << std::endl; 
+
+    std::cout << "  A) infinite radius" << std::endl; 
+    {
+		  std::vector<Point> groundTruth; 
+		  groundTruth.push_back(Point(0,-5)); 
+		  groundTruth.push_back(Point(3,-4)); 
+		  groundTruth.push_back(Point(4,-3)); 
+		  groundTruth.push_back(Point(5,0)); 
+		  groundTruth.push_back(Point(4,3));
+		  groundTruth.push_back(Point(3,4)); 
+		  groundTruth.push_back(Point(0,5)); 
+		  groundTruth.push_back(Point(-3,4)); 
+		  groundTruth.push_back(Point(-4,3)); 
+		  groundTruth.push_back(Point(-5,0)); 
+		  groundTruth.push_back(Point(-4,-3));
+		  groundTruth.push_back(Point(-3,-4)); 
+		  std::cout << "1 - Expected - handmade" << std::endl; 
+		  std::copy(groundTruth.begin(), groundTruth.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  std::vector<Point> ch;
+		  CircumcircleRadiusPredicate<> predicate; //by default infinite radius (denominator = 0)  
+		  closedGrahamScan( boundary.begin(), boundary.end(), std::back_inserter(ch), predicate ); 
+		  std::cout << "2 - Graham's convex hull of the boundary" << std::endl; 
+		  std::copy(ch.begin(), ch.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  if (ch.size() == groundTruth.size())
+		    if ( std::equal(groundTruth.begin(), groundTruth.end(), ch.begin()) )
+		      nbok++; 
+		  nb++; 
+		  std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+
+		  std::vector<Point> as; 
+		  alphaShapeNaive( circle, circle.getConvexHullVertex(), std::back_inserter(as), 50, predicate );
+		  as.erase(as.end()); 
+		  std::cout << "3 - Alpha shape" << std::endl; 
+		  std::copy(as.begin(), as.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  if (as.size() == groundTruth.size())
+		    if ( std::equal(groundTruth.begin(), groundTruth.end(), as.begin()) )
+		      nbok++; 
+		  nb++; 
+		  std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+		}
+		
+    std::cout << std::endl; std::cout << "  B) radius == 1" << std::endl; 
+		{
+		  std::cout << "Expected (boundary)" << std::endl; 
+		  std::copy(boundary.begin(), boundary.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  std::vector<Point> ch;
+		  CircumcircleRadiusPredicate<> predicate(1,1); //radius 1  
+		  closedGrahamScan( boundary.begin(), boundary.end(), std::back_inserter(ch), predicate ); 
+		  std::cout << "1-shape of the boundary" << std::endl; 
+		  std::copy(ch.begin(), ch.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  if (ch.size() == boundary.size())
+		    if ( std::equal(boundary.begin(), boundary.end(), ch.begin()) )
+		      nbok++; 
+		  nb++; 
+		  std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+		
+			std::vector<Point> as; 
+		  alphaShapeNaive( circle, circle.getConvexHullVertex(), std::back_inserter(as), 50, predicate );
+		  as.erase(as.end()); 
+		  std::cout << "Alpha shape" << std::endl; 
+		  std::copy(as.begin(), as.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  if (as.size() == ch.size())
+		    if ( std::equal(ch.begin(), ch.end(), as.begin()) )
+		      nbok++; 
+		  nb++; 
+		  std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+		}
+		
+		std::cout << std::endl; std::cout << "  B) radius == 3" << std::endl; 
+		{
+		  std::vector<Point> groundTruth; 
+		  groundTruth.push_back(Point(0,-5)); 
+		  groundTruth.push_back(Point(2,-4)); 
+		  groundTruth.push_back(Point(3,-4)); 
+		  groundTruth.push_back(Point(4,-3)); 
+		  groundTruth.push_back(Point(4,-2)); 
+		  groundTruth.push_back(Point(5,0));
+		  groundTruth.push_back(Point(4,2));  
+		  groundTruth.push_back(Point(4,3));
+		  groundTruth.push_back(Point(3,4)); 
+		  groundTruth.push_back(Point(2,4)); 
+		  groundTruth.push_back(Point(0,5)); 
+		  groundTruth.push_back(Point(-2,4)); 
+		  groundTruth.push_back(Point(-3,4)); 
+		  groundTruth.push_back(Point(-4,3)); 
+		  groundTruth.push_back(Point(-4,2)); 
+		  groundTruth.push_back(Point(-5,0)); 
+		  groundTruth.push_back(Point(-4,-2)); 
+		  groundTruth.push_back(Point(-4,-3));
+		  groundTruth.push_back(Point(-3,-4));
+		  groundTruth.push_back(Point(-2,-4)); 
+		  std::cout << "Expected" << std::endl; 
+		  std::copy(groundTruth.begin(), groundTruth.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  std::vector<Point> ch;
+		  CircumcircleRadiusPredicate<> predicate(9,1); //radius 3  
+		  closedGrahamScan( boundary.begin(), boundary.end(), std::back_inserter(ch), predicate ); 
+		  std::cout << "3-shape of the boundary" << std::endl; 
+		  std::copy(ch.begin(), ch.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  if (ch.size() == groundTruth.size())
+		    if ( std::equal(groundTruth.begin(), groundTruth.end(), ch.begin()) )
+		      nbok++; 
+		  nb++; 
+		  std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+	
+			std::vector<Point> as; 
+		  alphaShapeNaive( circle, circle.getConvexHullVertex(), std::back_inserter(as), 50, predicate );
+		  as.erase(as.end()); 
+		  std::cout << "Alpha shape" << std::endl; 
+		  std::copy(as.begin(), as.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+		  std::cout << std::endl; 
+
+		  if (as.size() == groundTruth.size())
+		    if ( std::equal(groundTruth.begin(), groundTruth.end(), as.begin()) )
+		      nbok++; 
+		  nb++; 
+		  std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+  	}
+  }
+std::cout << std::endl; 
   
   std::cout << "I) Alpha-shape on a simple circle" << std::endl; 
   {
@@ -202,7 +365,6 @@ int main()
 		  std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
   	}
   }
-
   //1 if at least one test failed
   //0 otherwise
   return (nb != nbok); 
