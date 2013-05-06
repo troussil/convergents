2fae588857a5f9ea49c491477d9709fbee628fb2
Homeostasie <thom@homeostasie.eu>
2013-05-06 16:49:54 +0200

    Positive alphashape start

---
+++ inc/PositiveAlphaShape.h
@@ -0,0 +1,204 @@
+#ifndef OutputSensitiveAlphaShape_h
+#define OutputSensitiveAlphaShape_h
+
+#include<cmath>
+
+#include"CircumcircleRadiusPredicate.h"
+#include "RayIntersectableStraightLine.h"
+
+#include "ConvexHullHelpers.h"
+
+/**
+ * Class implementing an on-line and ouput-sensitive algorithm
+ * that retrieves the vertices of the alpha-shape of all digital
+ * points lying inside an implicit shape, which is 'ray-intersectable',
+ * ie. the intersection between the shape boundary and
+ * a ray emanating from a given point along a given direction
+ * is computable.
+ *
+ * @tparam TShape a model of ray-intersectable shape.
+ * @tparam TPredicate a model of ternary predicate: 
+ * given three points, the operator() returns a bool.  
+ */
+template <typename TShape, typename TPredicate>
+class PositiveAlphaShape
+{
+  public:
+    /////////////////////// inner types /////////////////
+    typedef TShape Shape;
+    typedef typename Shape::Point Point;
+    typedef typename Shape::Vector Vector; //type redefinition
+    typedef TPredicate Predicate;
+    typedef std::deque<Point> Container;
+  
+  private:
+    /////////////////////// members /////////////////////
+    /**
+     * const reference on a shape
+     */
+    const Shape& myShape;
+    /**
+     * Predicate that returns 'true' if the radius 
+     * of the circumcircle of three given points
+     * is greater than 1/alpha, 'false' otherwise.
+     *
+     * NB. alpha is implicitely defined by the predicate. 
+     */
+    const Predicate& myPredicate; 
+
+  public:
+    ///////////////////// standard services /////////////
+    /**
+     * Standard constructor
+     * @param aShape any 'ray-intersectable' shape
+     * @param aPredicate any predicate
+     */
+    PositiveAlphaShape(const Shape& aShape, const Predicate& aPredicate)
+      : myShape(aShape), myPredicate(aPredicate) {}
+
+  private:
+    /**
+     * Copy constructor
+     * @param other other object to copy
+     */
+    PositiveAlphaShape(const PositiveAlphaShape& other) {}
+
+    /**
+     * Assignement operator
+     * @param other other object to copy
+     * @return reference on *this
+     */
+    PositiveAlphaShape& operator=(const PositiveAlphaShape& other)
+    { return *this; }
+
+  public:
+    /**
+     * Get Circle Predicate 
+     * @param aShape any 'ray-intersectable' shape
+     * @return aPredicate any predicate
+     */
+    Predicate getPredicate()
+    {
+      return ((*this).myPredicate);
+    }
+    /**
+     * Default destructor
+     */
+    ~PositiveAlphaShape() {}
+
+
+    ///////////////////// main methods ///////////////////
+  public:
+       
+
+    /**
+     * Given a vertex of the alpha-shape, 
+     * retrieves a sequence of consecutive 
+     * vertices of the alpha-shape
+     * in a counter-clockwise order. 
+     * 
+     * @param aPoint any vertex of the alpha-shape 
+     * @param res output iterator that stores the sequence of vertices
+     * @return the last retrieved vertex (not stored). 
+     */
+template <typename Container, typename Point>
+      void all(Container& aContainer, Point aPointa)
+      {
+
+        // Retrieve Convex Hull vertices
+        OutputSensitiveConvexHull<TShape> ch(myShape); 
+
+        std::vector<Point> it;
+        ch.all(aPointa, std::back_inserter(it));
+        
+    std::cout << "Convex Hull" << std::endl; 
+    std::copy(it.begin(), it.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+    std::cout << std::endl;
+    
+        int i = 0;
+
+        // Init point
+        Point pStart = it[i++];
+        Point pFol1  = it[i++];
+        Point pFol2  = it[i++];       
+       
+        it.push_back(pStart);
+        
+        // Init Container
+        aContainer.push_back(pStart);
+        bool cycle = false;
+     
+     
+//std::cout <<"IN : "<<it.size()<< std::endl;
+        
+        while( !cycle )
+        {
+std::cout <<"Enter : " <<i<<pStart<< pFol1 <<pFol2 << std::endl; 
+          // Add second point in the container
+          aContainer.push_back(pFol1);        
+          
+          // TRUE -- R_T(a,b1,b2)>R_alpha -- b1 not in the alpha-shape
+          if (myPredicate(pStart, pFol1, pFol2))
+          {
+std::cout <<"POP --> " << pFol1 << aContainer.front()<<std::endl;
+
+            // remove b1
+            if (pFol1 == aContainer.front())
+            {
+              aContainer.pop_front();
+              cycle = true;
+            }
+            aContainer.pop_back();
+          }
+          // FALSE --  R_T(a,b1,b2)<R_alpha -- b1 in the alpha-shape
+          else
+          {
+std::cout <<"PUSH --> " << pFol1 << aContainer.front()<<std::endl;
+            // New start
+            if (pFol1 == aContainer.front())
+            {
+              cycle = true;
+              aContainer.pop_back();
+            }
+            else
+            {
+              pStart = pFol1;
+              it.push_back(pFol1);
+            }
+          }
+          // update Points
+          pFol1 = pFol2;
+          pFol2 = it[i++];    
+//std::cout <<"OUT ; "<<i<<pStart<< pFol1 <<pFol2 << std::endl;    
+        }
+             
+      }//end proc
+
+/*template <typename Container, typename Point, typename Predicate>
+void updateConvexHull(Container& container, const Point& newPoint, const Predicate& p)
+{
+  Point Q = container.back(); 
+  container.pop_back(); 
+  if (container.size() != 0) 
+    {
+      Point R = container.back(); 
+      //std::cout << " signed area of " << newPoint << " " << Q << " " << R << " : " << signedArea(newPoint,Q,R) << std::endl; 
+      while ( ( p(newPoint,Q,R) )&&(container.size() != 0) )
+	{
+	  //remove Q
+	  //std::cout << " remove from back" << Q << std::endl; 
+	  Q = R; 
+	  container.pop_back(); 
+	  if (container.size() != 0) 
+	    R = container.back(); 
+	}
+      //add Q
+      container.push_back(Q); 
+    }
+}*/
+
+
+}; 
+#endif
+
+
---
+++ stests/testPositiveAlphaShape.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+//containers and iterators
+#include <iterator>
+#include <vector>
+#include <deque>
+// random
+#include <cstdlib>
+#include <ctime>
+
+#include <cmath>
+// Core geometry
+#include "../inc/PointVector2D.h"
+// Circle
+#include "../inc/RayIntersectableCircle.h"
+// Convex Hull
+
+#include "../inc/OutputSensitiveConvexHull.h"
+
+// Alpha-shape
+#include "../inc/PositiveAlphaShape.h"
+
+// BigInteger
+#include <DGtal/base/Common.h>
+
+//uncomment to use in DEBUG_VERBOSE mode 
+#define DEBUG_VERBOSE
+
+typedef PointVector2D<int> Point; //type redefinition
+typedef PointVector2D<int> Vector; //type redefinition
+typedef std::deque<Point> Container;
+
+//////////////////////////////////////////////////////////////////////
+template <typename Shape, typename Container, typename Predicate>
+void PositiveAlpha(const Shape& aShape, Container& container, const Predicate& aPredicate)
+{
+
+  PositiveAlphaShape<Shape, Predicate> ch(aShape, aPredicate);
+  ch.all(container, aShape.getConvexHullVertex()); 
+}
+///////////////////////////////////////////////////////////////////////
+/**
+ * @brief Procedure that checks whether the 
+ * output-sensitive algorithm returns the same
+ * alpha-shape as the tracking-based algorithm
+ * for a given circle. 
+ * 
+ * @param aCircle
+ * @param aPredicate  
+ * 
+ * @return 'true' if the test passed, 'false' otherwise
+ * 
+ * @tparam
+ * @tparam CircumcircleRadiusPredicate
+ */
+  template<typename Circle, typename CircumcircleRadiusPredicate>
+bool test(const Circle aCircle, const CircumcircleRadiusPredicate& aPredicate)
+{
+
+  Container container; 
+  PositiveAlpha( aCircle, container, aPredicate ); 
+  
+  std::cout << "Get" << std::endl; 
+  std::copy(container.begin(), container.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
+  std::cout << std::endl; 
+  return false;
+}
+
+
+///////////////////////////////////////////////////////////////////////
+int main() 
+{
+  typedef PointVector2D<int> Point; //type redefinition
+  typedef PointVector2D<int> Vector; //type redefinition
+  typedef RayIntersectableCircle<Point> Circle; 
+
+  int nbok = 0; //number of tests ok
+  int nb = 0;   //total number of tests
+
+  #ifdef DEBUG_VERBOSE
+  std::cout << std::endl; 
+  std::cout << "I) Alpha-shape on a simple circle" << std::endl; 
+  #endif
+  
+  //  (0,2), (1,3), (2,4), (2,5), (2,6), (2,7), (1,8), (-1,9), (-3,8), (-4,7), (-4,6), (-4,5), (-4,4), (-4,3), (-2,2), (-1,2), (0,2), 
+  //-- Disk[ Center : (-1.27273, 5.40909 ), Radius : 3.63892 ] | Points : (2,7)(-4,3)(0,2) - First vertex : (0,2)
+  //10/2
+  {
+    Point pta = Point(0,6);
+    Point ptb = Point(-4,3);
+    Point ptc = Point(0,2);
+    Circle circle( pta, ptb, ptc );
+
+    #ifdef DEBUG_VERBOSE
+    std::cout << "-- Disk[ Center : (" << circle.getCenterX() << ", " 
+      << circle.getCenterY()<< " ), Radius : " << circle.getRadius()
+      << " ] | Points : "<< pta<< ptb<< ptc<< " - First vertex : " 
+      << std::endl;
+
+    std::cout << " ----------- Next predicate ----------- " << std::endl; 
+    std::cout << std::endl;
+    #endif
+
+    CircumcircleRadiusPredicate<> predicate(10,2);
+    
+    #ifdef DEBUG_VERBOSE
+    std::cout << "Radius predicate : Num2 / Den2 : 10/2"<< std::endl;
+    #endif
+
+    if (test(circle, predicate))
+      nbok++; 
+    nb++; 
+
+    std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+  }
+  //(4,2)(2,1)(6,-5) - (4,2)(0,-6)(10,-14) - (7,8)(-1,-1)(3,-8) - (5,3)(2,2)(4,-7)
+  // Circle parameter : ax + by + c(x^2 + y^2)   
+  
+  
+  {
+  int R = 10;
+  
+  //random value
+  srand ( time(NULL) );
+  
+  int c = -25;
+  
+  //int a = - rand() %(2*c);
+  //int b = - rand() %(2*c);
+  
+  int a = 20;
+  int b = 32;  
+  
+  int d = ( a*a + b*b - 4*R*R*c*c)/(4*c);;
+
+  
+  // We fixed c = -20.  pt_c = [-a/2c ; -b/2c] and R² = (a^2 + b^2 - 4*c*d)/(4c²)
+	// so 4c²*R² = a² + b² - 4*c*d <=>  = (a² + b² - 4c²*R²)/4*c = d
+
+	    // Create a circle from the Euclidian parameter a, b, c, d.
+	  Circle circle( a, b, c, d );	
+	  
+	   #ifdef DEBUG_VERBOSE
+    std::cout << "-- Disk[ Center : (" << circle.getCenterX() << ", " 
+      << circle.getCenterY()<< " ), Radius : " << circle.getRadius()
+      << std::endl;
+
+    std::cout << " ----------- Next predicate ----------- " << std::endl; 
+    std::cout << std::endl;
+    #endif
+	  
+    CircumcircleRadiusPredicate<DGtal::BigInteger> predicate5(110,1);
+    
+    #ifdef DEBUG_VERBOSE
+    std::cout << "Radius predicate : Num2 / Den2 : 110/1"<< std::endl;
+    #endif
+
+    if (test(circle, predicate5))
+    {nbok++;} 
+    nb++; 
+
+    std::cout << "(" << nbok << " tests passed / " << nb << " tests)" << std::endl;
+    }
+
+  //1 if at least one test failed
+  //0 otherwise
+  return (nb != nbok); 
+}
