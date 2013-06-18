463c14752a784d36edfd8ee65389efe1637dd128
Homeostasie <thom@homeostasie.eu>
2013-06-18 11:32:24 +0200

    add alpha-shape (>0, <0) display

--- stools/toolDisplay.cpp
+++ stools/toolDisplay.cpp
@@ -29,26 +29,15 @@ using namespace DGtal;
 #include "../inc/ExactRayIntersectableCircle.h"
 #include "../inc/OutputSensitiveConvexHull.h"
 #include "../inc/IncrementalNegativeAlphaShape.h"
+#include "../inc/BottomUpPositiveAlphaShape.h"
 #include "../inc/ConvexHullHelpers.h"
 #include "../inc/CircumcircleRadiusPredicate.h"
 
-///////////////////////////////////////////////////////////////////////////////
-template <typename Shape, typename Point, typename OutputIterator>
-void convexHull(const Shape& aShape, const Point& aStartingPoint, 
-		OutputIterator res)
-{
-  OutputSensitiveConvexHull<Shape> ch(aShape); 
-  //get the first vertex
-  Point tmp = aStartingPoint; 
-  do {
-    //store the current vertex
-    *res++ = tmp; 
-    //get the next vertex
-    tmp = ch.next(tmp); 
-    //while it is not the first one
-  } while (tmp != aStartingPoint); 
-}
-
+typedef PointVector<2,int> Point; //DGtal point redefinition
+typedef PointVector<2,int> Vector; //DGtal point redefinition
+typedef ExactRayIntersectableCircle<Point,DGtal::BigInteger> Circle; //Circle
+typedef CircumcircleRadiusPredicate<DGtal::BigInteger> Predicate;
+typedef std::deque<Point> Container; 
 ///////////////////////////////////////////////////////////////////////////////
 template <typename ForwardIterator>
 void display(const ForwardIterator& itb, const ForwardIterator& ite, 
@@ -87,226 +76,279 @@ void display(const ForwardIterator& itb, const ForwardIterator& ite,
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-template<typename Circle, typename I>
-void grahamProcedure(const Circle& aCircle, const I& num2, const I& den2)
+// Comput Convex Hull
+  template <typename Shape>
+void convexHull(const Shape& aShape, std::vector<Point> res, bool aVertOnEdges)
 {
-  typedef typename Circle::Point Point; 
-  typedef typename Circle::Vector Vector; 
-
-
-  //computation of the boundary and alpha-shape
+  //computation of the alpha-shape
   std::chrono::time_point<std::chrono::system_clock> ta, tb;
   ta = std::chrono::system_clock::now();
 
-  //- boundary
-  std::vector<Point> boundary; 
-  Vector dir(1,0); 
-  closedTracking( aCircle, aCircle.getConvexHullVertex(), dir, std::back_inserter(boundary) ); 
-
-  //- alpha-shape
-  CircumcircleRadiusPredicate<> predicate(num2,den2);
-  std::vector<Point> ch; 
-  closedGrahamScan( boundary.begin(), boundary.end(), std::back_inserter(ch), predicate ); 
+  OutputSensitiveConvexHull<Shape> ch(aShape);
+  ch.all(std::back_inserter(res), aVertOnEdges);
 
   tb = std::chrono::system_clock::now();
+  
   std::cout << "# Time: "
-	    << std::chrono::duration_cast<std::chrono::microseconds>(tb-ta).count() 
-	    << " ms " << std::endl;
-  std::cout << "# Numb: " 
-	    << ch.size() 
-	    << " vertices " << std::endl; 
+    << std::chrono::duration_cast<std::chrono::microseconds>(tb-ta).count()
+    << " ms " << std::endl;
+  std::cout << "# Numb: "
+    << res.size()
+    << " vertices " << std::endl;
 
   //display in the standard output
-  std::cout << "Graham's (" 
-	    << ( std::sqrt( (double) num2) / std::sqrt( (double) den2) )
-	    << ")-shape of the boundary" << std::endl; 
-  std::copy(ch.begin(), ch.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
-  std::cout << std::endl; 
-  
+  std::cout << "Convex Hull"<< std::endl;
+  std::copy(res.begin(), res.end(), std::ostream_iterator<Point>(std::cout, ", ") );
+  std::cout << std::endl;
+
   //display in an output file
-  std::string filename = "alphaShapeByGraham"; 
-  display(ch.begin(), ch.end(), filename); 
-  std::cout << "# Display stored in file " 
-	    << filename << ".eps" << std::endl;
+  std::string filename = "OutputSensitiveConvexHull";
+  display(res.begin(), res.end(), filename);
+  std::cout << "# Display stored in file "
+    << filename << ".eps" << std::endl;
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-template<typename Circle, typename I>
-void outputSensitiveProcedure(const Circle& aCircle, const I& num2, const I& den2)
+// Output Sensitive Alpha-Shape, alpha <= 0
+template <typename Shape, typename Predicate>
+void NegAlphaShape(const Shape& aShape, std::vector<Point> res, const Predicate& aPredicate)
 {
-  typedef typename Circle::Point Point; 
-  typedef typename Circle::Vector Vector; 
+  typedef typename Circle::Point Point;
+  typedef typename Circle::Vector Vector;
 
-  std::cout << "output sensitive proc" << std::endl; 
-
-  typedef CircumcircleRadiusPredicate<DGtal::BigInteger> Predicate; 
-  Predicate predicate(num2, den2);
-  IncrementalNegativeAlphaShape<Circle, Predicate> as(aCircle, predicate);
+  std::cout << "output sensitive proc" << std::endl;
 
   //computation of the alpha-shape
   std::chrono::time_point<std::chrono::system_clock> ta, tb;
   ta = std::chrono::system_clock::now();
 
-  std::vector<Point> ch; 
-  as.all( std::back_inserter(ch) ); 
+  IncrementalNegativeAlphaShape<Shape, Predicate> nas(aShape, aPredicate);
+  nas.all(std::back_inserter(res));
 
   tb = std::chrono::system_clock::now();
   std::cout << "# Time: "
-	    << std::chrono::duration_cast<std::chrono::microseconds>(tb-ta).count() 
-	    << " ms " << std::endl;
-  std::cout << "# Numb: " 
-	    << ch.size() 
-	    << " vertices " << std::endl; 
+    << std::chrono::duration_cast<std::chrono::microseconds>(tb-ta).count()
+    << " ms " << std::endl;
+  std::cout << "# Numb: "
+    << res.size()
+    << " vertices " << std::endl;
 
   //display in the standard output
-  std::cout << "Output-sensitive (" 
-	    << ( std::sqrt( (double) num2) / std::sqrt( (double) den2) )
-	    << ")-shape " << std::endl; 
-  std::copy(ch.begin(), ch.end(), std::ostream_iterator<Point>(std::cout, ", ") ); 
-  std::cout << std::endl; 
-  
+  std::cout << "Output-sensitive (-"
+    << ( aPredicate.getNum2() / (double)aPredicate.getDen2() )
+    << ")-shape " << std::endl;
+  std::copy(res.begin(), res.end(), std::ostream_iterator<Point>(std::cout, ", ") );
+  std::cout << std::endl;
+
   //display in an output file
-  std::string filename = "OutputSensitiveAlphaShape"; 
-  display(ch.begin(), ch.end(), filename); 
-  std::cout << "# Display stored in file " 
-	    << filename << ".eps" << std::endl;
+  std::string filename = "OutputSensitiveAlphaShape";
+  display(res.begin(), res.end(), filename);
+  std::cout << "# Display stored in file "
+    << filename << ".eps" << std::endl;
+    
+}
+
+//////////////////////////////////////////////////////////////////////
+// Alpha-Shape, alpha > Rmin
+  template <typename Shape, typename Point, typename Container, typename Predicate>
+void PosAlphaShape(const Shape& aShape, const Point& aStart, Container& res, const Predicate& aPredicate)
+{
+
+  //computation of the alpha-shape
+  std::chrono::time_point<std::chrono::system_clock> ta, tb;
+  ta = std::chrono::system_clock::now();
+  
+  BottomUpPositiveAlphaShape<Shape, Predicate> pas(aShape, aPredicate);
+  pas.all(res, aStart);
+
+  tb = std::chrono::system_clock::now();
+  std::cout << "# Time: "
+    << std::chrono::duration_cast<std::chrono::microseconds>(tb-ta).count()
+    << " ms " << std::endl;
+  std::cout << "# Numb: "
+    << res.size()
+    << " vertices " << std::endl;
+
+  //display in the standard output
+  std::cout << "("
+    << ( aPredicate.getNum2() / (double)aPredicate.getDen2() )
+    << ")-shape " << std::endl;
+  std::copy(res.begin(), res.end(), std::ostream_iterator<Point>(std::cout, ", ") );
+  std::cout << std::endl;
+
+  //display in an output file
+  std::string filename = "PositiveAlphaShape";
+  display(res.begin(), res.end(), filename);
+  std::cout << "# Display stored in file "
+    << filename << ".eps" << std::endl;
+
 
 }
+
 
 ///////////////////////////////////////////////////////////////////////////////
-template<typename Circle, typename I>
-void mainProcedure(const Circle& aCircle, const I& num2, const I& den2, 
-		   const std::string& methodName)
+// Main Procedure///////////////////////////////////////////////////////////////////////////////
+  template<typename Shape, typename Point, typename Predicate>
+void mainProcedure(const Shape& aShape, const Point& aStart, const Predicate& aPredicate,
+    const bool& aVertOnEdges, const int& methodId)
 {
-  if (methodName == "Har-Peled")
-    outputSensitiveProcedure(aCircle, num2, den2); 
-  else 
-    grahamProcedure(aCircle, num2, den2); 
+  if (methodId == 1)
+  {
+    std::vector<Point> res;
+    convexHull(aShape, res, aVertOnEdges);
+  }
+  else
+  {
+    if (methodId == 3)
+    {
+      Container container;
+
+      PosAlphaShape( aShape, aStart, container, aPredicate );
+    }
+    else
+    {
+      std::vector<Point> res;
+      NegAlphaShape(aShape, (res), aPredicate);
+    }
+  }
 }
+
+
 
 ///////////////////////////////////////////////////////////////////////////////
 int main( int argc, char** argv )
 {
-  // parse command line ----------------------------------------------
+// parse command line ----------------------------------------------
   po::options_description general_opt("Allowed options are: ");
   general_opt.add_options()
     ("help,h", "display this message")
-    ("algo", po::value<std::string>()->default_value("Har-Peled"), "algorithm: either 'Har-Peled' or 'other'(Graham)")
+    ("algorithm,a,", po::value<int>()->default_value(1), "algorithm: Convex-Hull (=1), Negative Alpha-Shape (=2) or Positive Alpha-Shape (=3)")
     ("numerator2,n", po::value<int>()->default_value(1), "squared numerator of 1/alpha")
     ("denominator2,m", po::value<int>()->default_value(0), "squared denominator of 1/alpha")
-    ("radius,R",  po::value<int>(), "Radius of the disc" )
-    ("a",  po::value<int>(), "a parameter of the disc" )
-    ("b",  po::value<int>(), "b parameter of the disc" )
-    ("c",  po::value<int>(), "c parameter of the disc" )
-    ("d",  po::value<int>(), "d parameter of the disc" )
-    ("x1",  po::value<int>(), "x-coordinate of the first point" )
-    ("y1",  po::value<int>(), "y-coordinate of the first point" )
-    ("x2",  po::value<int>(), "x-coordinate of the second point" )
-    ("y2",  po::value<int>(), "y-coordinate of the second point" )
-    ("x3",  po::value<int>(), "x-coordinate of the third point" )
-    ("y3",  po::value<int>(), "y-coordinate of the third point" );
-  
+    ("edgeVertices,ev", po::value<bool>()->default_value(false), "Take (true), or not (true) the vertices which lie on the edge")
+    ("radius,R", po::value<int>(), "Radius of the disc" )
+    ("a", po::value<int>(), "a parameter of the disc" )
+    ("b", po::value<int>(), "b parameter of the disc" )
+    ("c", po::value<int>(), "c parameter of the disc" )
+    ("d", po::value<int>(), "d parameter of the disc" )
+    ("x1", po::value<int>(), "x-coordinate of the first point" )
+    ("y1", po::value<int>(), "y-coordinate of the first point" )
+    ("x2", po::value<int>(), "x-coordinate of the second point" )
+    ("y2", po::value<int>(), "y-coordinate of the second point" )
+    ("x3", po::value<int>(), "x-coordinate of the third point" )
+    ("y3", po::value<int>(), "y-coordinate of the third point" );
+
   bool parseOK=true;
   po::variables_map vm;
   try{
-    po::store(po::parse_command_line(argc, argv, general_opt), vm);  
+    po::store(po::parse_command_line(argc, argv, general_opt), vm);
   }catch(const std::exception& ex){
     parseOK=false;
-    trace.info()<< "Error checking program options: "<< ex.what()<< std::endl; 
+    trace.info()<< "Error checking program options: "<< ex.what()<< std::endl;
   }
-  po::notify(vm);    
+  po::notify(vm);
   if(!parseOK || vm.count("help")||argc<=1)
-    {
-      trace.info()<< "Display alpha-shape of grid points lying inside the specified disc" 
-                  <<std::endl << "Basic usage: "<<std::endl
-		  << "\ttoolDisplay -R 15" << std::endl
-		  << general_opt << "\n";
-      return 0;
-    }
+  {
+    trace.info()<< "Display alpha-shape of grid points lying inside the specified disc"
+      <<std::endl << "Basic usage: "<<std::endl
+      << "\ttoolDisplay -R 15" << std::endl
+      << general_opt << "\n";
+    return 0;
+  }
  
-  typedef PointVector<2,int> Point; //DGtal point redefinition
-  typedef PointVector<2,int> Vector; //DGtal point redefinition
-  typedef ExactRayIntersectableCircle<Point,DGtal::BigInteger> Circle; //Circle
-
-  std::string methodName = vm["algo"].as<std::string>(); 
+ 
+  int methodId = vm["algorithm"].as<int>();
   int num2 = vm["numerator2"].as<int>();
   int den2 = vm["denominator2"].as<int>();
 
+  bool edgeVertices = vm["edgeVertices"].as<bool>();
+  
+  bool positive = (methodId == 3);
+   
   if (vm.count("radius"))
-    { //if radius option specified
-      int radius = vm["radius"].as<int>();
-      std::cout << "Disc of radius " << radius << std::endl; 
+  { //if radius option specified
+    int radius = vm["radius"].as<int>();
+    std::cout << "Disc of radius " << radius << std::endl;
 
-      Circle circle( Point(radius,0), Point(0,radius), Point(-radius,0) );
+  
+    Circle circle( Point(radius,0), Point(0,radius), Point(-radius,0) );
+    CircumcircleRadiusPredicate<> predicate(num2, den2, positive);
 
-      mainProcedure( circle, num2, den2, methodName ); 
+    mainProcedure( circle, Point(0,-radius), predicate, edgeVertices, methodId );
+  }
+  else
+  {
+    if ( vm.count("x1") &&
+        vm.count("y1") &&
+        vm.count("x2") &&
+        vm.count("y2") &&
+        vm.count("x3") &&
+        vm.count("y3") )
+    {
+      //if point options specified
+      std::cout << "Display disc passing through 3 points " << std::endl;
+      Point p, q, r;
+      p[0] = vm["x1"].as<int>();
+      p[1] = vm["y1"].as<int>();
+      q[0] = vm["x2"].as<int>();
+      q[1] = vm["y2"].as<int>();
+      r[0] = vm["x3"].as<int>();
+      r[1] = vm["y3"].as<int>();
+
+      Point pStart = p;
+      
+      //orientation test
+      Vector u = p-q;
+      Vector v = r-q;
+      if ( (u[0]*v[1] - u[1]*v[0]) >= 0)
+      {
+        std::cerr << "Your three points should be in a counter-clockwise order (and not collinear)" << std::endl;
+        return 1;
+      }
+
+      Circle circle( p, q, r );
+      CircumcircleRadiusPredicate<> predicate(num2, den2, positive);
+
+      mainProcedure( circle, p, predicate, edgeVertices, methodId );
+      
     }
-  else 
-    { 
-      if ( vm.count("x1") && 
-	   vm.count("y1") &&
-	   vm.count("x2") &&
-	   vm.count("y2") && 
-	   vm.count("x3") &&
-	   vm.count("y3") )
-	{
-	  //if point options specified
-	  std::cout << "Display disc passing through 3 points " << std::endl;
-	  Point p, q, r; 
-	  p[0] = vm["x1"].as<int>();
-	  p[1] = vm["y1"].as<int>();
-	  q[0] = vm["x2"].as<int>();
-	  q[1] = vm["y2"].as<int>();
-	  r[0] = vm["x3"].as<int>();
-	  r[1] = vm["y3"].as<int>();
+    else
+    {
+      if ( vm.count("a") &&
+          vm.count("b") &&
+          vm.count("c") &&
+          vm.count("d") )
+      {
+        int a = vm["a"].as<int>();
+        int b = vm["b"].as<int>();
+        int c = vm["c"].as<int>();
+        int d = vm["d"].as<int>();
+        std::cout << "Disc of parameters "
+          << a << " "
+          << b << " "
+          << c << " "
+          << d << std::endl;
 
-	  //orientation test
-	  Vector u = p-q; 
-	  Vector v = r-q; 
-	  if ( (u[0]*v[1] - u[1]*v[0]) >= 0) 
-	    {
-	      std::cerr << "Your three points should be in a counter-clockwise order (and not collinear)" << std::endl;
-	      return 1; 
-	    }
-
-	  Circle circle( p, q, r );
-
-	  mainProcedure( circle, num2, den2, methodName ); 
-	}
+        //orientation test
+        if ( ( c >= 0 )&&( d >= c) )
+        {
+          std::cerr << "Parameter c should be strictly negative (0 = straight line; >0 = bad orientation)" << std::endl;
+          return 1;
+        }
+        
+        Circle circle( a, b, c, d );
+        CircumcircleRadiusPredicate<> predicate(num2, den2, positive);
+       
+        mainProcedure( circle, circle.getConvexHullVertex(), predicate, edgeVertices, methodId );
+         
+      }
       else
-	{
-	  if ( vm.count("a") && 
-	       vm.count("b") &&
-	       vm.count("c") &&
-	       vm.count("d") )
-	    {
-	      int a = vm["a"].as<int>();
-	      int b = vm["b"].as<int>();
-	      int c = vm["c"].as<int>();
-	      int d = vm["d"].as<int>();
-	      std::cout << "Disc of parameters " 
-			<< a << " "
-			<< b << " "
-			<< c << " " 
-			<< d << std::endl; 
-
-	      //orientation test
-	      if ( ( c >= 0 )&&( d >= c) )  
-		{
-		  std::cerr << "Parameter c should be strictly negative (0 = straight line; >0 = bad orientation)" << std::endl;
-		  return 1; 
-		}
-
-	      Circle circle( a, b, c, d );
-
-	      mainProcedure( circle, num2, den2, methodName ); 
-
-	    }
-	  else
-	    std::cerr << "Bad input arguments. Try option --help. " << std::endl; 
-	}
-
+        std::cerr << "Bad input arguments. Try option --help. " << std::endl;
     }
+
+  }
 
   return 0;
 }
+
